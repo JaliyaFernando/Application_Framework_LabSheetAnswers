8887a273905a2033a4220e9e2bee7b7566c1dc22
Jaliya <jaliyalaksirifernando@gmail.com>
2019-05-24 23:46:10 +0530

    add post and get methods to app.js

--- Lab05/Part01/app.js
+++ Lab05/Part01/app.js
@@ -17,3 +17,33 @@ app.listen(3000, err =>{
     console.log('Server is listen on port: 3000');
 })
 
+app.post('/add', (req,res) => {
+    const reqParam = req.body;
+
+    const user = new userObject(reqParam.fname,reqParam.lname,new Date(reqParam.bDay), Date.now());
+
+    userArr.push(user);
+    res.status(200).send({message:"User added to the array", data:user});
+});
+
+app.get('/all', (req,res) => {
+   try {
+       res.status(200).send({data:userArr});
+   }catch (e) {
+       res.status(500).send({message:e});
+
+   }
+});
+
+app.get('/userbyid/:Id', (req,res) => {
+    try {
+        const index = userArr.findIndex( x => x.id == req.params.Id);
+        if(index > -1){
+            res.status(200).send(userArr[index]);
+        }else {
+            res.status(404).send({message:'Invalid ID'});
+        }
+    }catch (e) {
+        res.status(500).send({message:e});
+    }
+});
